d7aa63a20fb9689474b7ba577d078e13dbe11792
jules klu <jules.klu@talan.com>
2022-11-16 18:45:45 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <nav class="navbar bg-light shadow sticky-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Deploy on AWS</a>
+      </div>
+    </nav>
+
+    <div class="mt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="..." class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+</template>
